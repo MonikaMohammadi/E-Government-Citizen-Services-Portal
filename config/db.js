706472bd945341c707e4a84592aa2106bae13da8
MonikaMohammadi <monicamohammadi456@gmail.com>
2025-10-07 10:13:02 +0430

    resolve the deploy issue

--- config/db.js
+++ config/db.js
@@ -1,13 +1,9 @@
 const { Pool } = require('pg')
 require('dotenv').config();
 
-
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 })
 
 
